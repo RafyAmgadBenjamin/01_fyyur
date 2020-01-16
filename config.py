71f08ec8ae0ebd23e40d29ef2afd6a12443329f0
Rafy <rafy.amgadbenjamin@gmail.com>
2020-01-16 19:38:35 +0200

    Migration intialization

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -11,3 +12,6 @@ DEBUG = True
 
 # TODO IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = "postgresql://rafy:admin@localhost:5432/fyyurapp"
+
+# Disapplying SQLALCHEMY_TRACK_MODIFICATIONS
+SQLALCHEMY_TRACK_MODIFICATIONS = False
