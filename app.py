325c6d5fb4097d35bc01cb325aecad0c5b65ddb2
Rafy <rafy.amgadbenjamin@gmail.com>
2020-01-20 22:47:26 +0200

    Code cleaning

--- app.py
+++ app.py
@@ -377,17 +377,10 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
+  # TODO-done: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  # response={
-  #   "count": 1,
-  #   "data": [{
-  #     "id": 4,
-  #     "name": "Guns N Petals",
-  #     "num_upcoming_shows": 0,
-  #   }]
-  # }
+
   search_term=request.form.get('search_term', '')
   search_term=search_term.strip()  #removing the spaces form the beginning and the end
   search_query = '%{0}%'.format(search_term) # prepare the query 
