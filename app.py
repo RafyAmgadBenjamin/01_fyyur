37fec1782d092f9581e360db7328b4a7203c71e1
Rafy <rafy.amgadbenjamin@gmail.com>
2020-01-20 16:53:43 +0200

    working on deletion button on JS

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ class Venue(db.Model):
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean,nullable=False,default=False)
     seeking_description = db.Column(db.String(120))
-    shows = db.relationship("Show", backref="venue", lazy=True) 
+    shows = db.relationship("Show", backref="venue", lazy=True, passive_deletes=True) 
     #Creating the one to many relation with the show class
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
